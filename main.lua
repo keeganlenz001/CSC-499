0c0ec41dc1dac83473878a7fc7351fa168ad3b25
Keegan Lenz <Keegan.lenz@student.cart.org>
2025-02-11 21:30:15 -0800

    falling block with board and array

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 WIDTH = 80
 HEIGHT = 160
-GRID = WIDTH / 10
+GRID = 8
+
+ROWS = 20
+COLUMNS = 10
+
 SCALE = 4
 
 SCALED_GRID = GRID * SCALE
@@ -14,36 +18,76 @@ function love.load()
         x = 0,
         y = 0
     }
+
+    board = {}
+    for i = 1, ROWS do
+        table.insert(board, {})
+        for j = 1, COLUMNS do
+            table.insert(board[i], 0)
+        end
+    end
+
+    board[1][1] = 1
+
+    T_PIECE = {
+        
+    }
 end
 
 function love.draw()
-    for i = 1, WIDTH / GRID do
+    for i = 1, COLUMNS + 1 do
         love.graphics.line(i * SCALED_GRID, 0, i * SCALED_GRID, HEIGHT * SCALED_GRID)
     end
-    for i = 1, HEIGHT / GRID do
+    for i = 1, ROWS + 1 do
         love.graphics.line(0, i * SCALED_GRID, WIDTH * SCALED_GRID, i * SCALED_GRID)
     end
 
-    love.graphics.rectangle("fill", block.x, block.y, SCALED_GRID, SCALED_GRID)
+    for i = 1, table.getn(board) do
+        for j = 1, table.getn(board[i]) do
+            if board[i][j] == 1 then
+                love.graphics.rectangle("fill", (j - 1) * SCALED_GRID, (i - 1) * SCALED_GRID, SCALED_GRID, SCALED_GRID)
+            end
+        end
+    end
+end
+
+function check_xdirection(dir)
+    for i = 1, table.getn(board) do
+        for j = 1, table.getn(board[i]) do
+            if board[i][j] == 1 and j + dir > 0 and j + dir < COLUMNS + 1 then
+                if board[i][j + dir] == 0 then
+                    board[i][j] = 0
+                    board[i][j + dir] = 1
+                    break
+                end
+            end
+        end
+    end
 end
 
 function love.update(dt)
     tick = tick + dt
 
     if tick > 1 then
-        block.y = block.y + SCALED_GRID
-        tick = 0
+        for i = 1, table.getn(board) do
+            for j = 1, table.getn(board[i]) do
+                if board[i][j] == 1 and i + 1 < ROWS + 1 then
+                    if board[i + 1][j] == 0 then
+                        board[i][j] = 0
+                        board[i + 1][j] = 1
+                        tick = 0
+                        return
+                    end
+                end
+            end
+        end
     end
 
     function love.keypressed(key)
         if key == "a" or key == "left" then
-            if block.x - SCALED_GRID >= 0 then
-                block.x = block.x - SCALED_GRID
-            end
+            check_xdirection(-1)
         elseif key == "d" or key == "right" then
-            if block.x + SCALED_GRID < WIDTH * SCALE then
-                block.x = block.x + SCALED_GRID
-            end
+            check_xdirection(1)
         end
     end
 end
