3b51c1235257f767c26a4ad3bd5049d88a84c120
Keegan Lenz <Keegan.lenz@student.cart.org>
2025-03-08 15:55:03 -0800

    beginning 3D conversion

--- main.lua
+++ main.lua
@@ -1,132 +1,185 @@
-WIDTH = 80
-HEIGHT = 160
+BOARD_WIDTH = 80
+BOARD_HEIGHT = 160
+PADDING = 16
 GRID = 8
 
 ROWS = 20
 COLUMNS = 10
+DEPTH = 4
 
 ROWS_BUFFER = 3
 COLUMNS_BUFFER = 3
 ROWS_OFFSET = 2
 COLUMNS_OFFSET = 2
 
-SCALE = 4
+SCALE = 3
 
 SCALED_GRID = GRID * SCALE
 
-love.window.setMode(WIDTH * SCALE, HEIGHT * SCALE)
+WIDTH = (BOARD_WIDTH * DEPTH * SCALE) + (PADDING * (DEPTH + 1))
+HEIGHT = (BOARD_HEIGHT * SCALE) + (PADDING * 2)
+love.window.setMode(WIDTH, HEIGHT)
 
 function love.load()
     tick = 0
 
     board = {}
-    for i = 1, ROWS + ROWS_BUFFER do
+    for i = 1, DEPTH do
         table.insert(board, {})
-        for j = 1, COLUMNS + COLUMNS_BUFFER do
-            table.insert(board[i], 0)
+        for j = 1, ROWS + ROWS_BUFFER do
+            table.insert(board[i], {})
+            for k = 1, COLUMNS + COLUMNS_BUFFER do
+                table.insert(board[i][j], 0)
+            end
         end
     end
 
+    local function get_shape3D(shape)
+        local shape3D = {}
+        shape3D[1] = shape
+
+        for depth = 2, #shape do
+            shape3D[depth] = {}
+            for i = 1, #shape do
+                shape3D[depth][i] = {}
+                for j = 1, #shape[i] do
+                    shape3D[depth][i][j] = 0
+                end
+            end
+        end
+
+        return shape3D
+    end
+
     function new_T_PIECE()
+        local shape = {
+            {0, 0, 0},
+            {1, 1, 1},
+            {0, 1, 0}
+        }
+
         return {
-            shape ={
-                {0, 0, 0},
-                {1, 1, 1},
-                {0, 1, 0}
-            },
-            position = {x = 5, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 5, y = 0, z = 0}
         }
     end
 
     function new_J_PIECE() 
+        local shape = {
+            {0, 0, 0},
+            {1, 1, 1},
+            {0, 0, 1},
+        }
+
         return {
-            shape = {
-                {0, 0, 0},
-                {1, 1, 1},
-                {0, 0, 1},
-            },
-            position = {x = 5, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 5, y = 0, z = 0}
         }
     end
 
     function new_Z_PIECE()
+        local shape = {
+            {0, 0, 0},
+            {1, 1, 0},
+            {0, 1, 1}
+        }
+
         return {
-            shape = {
-                {0, 0, 0},
-                {1, 1, 0},
-                {0, 1, 1}
-            },
-            position = {x = 5, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 5, y = 0, z = 0}
         }
     end
 
     function new_O_PIECE()
+        local shape = {
+            {0, 0, 0, 0},
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0}
+        }
+
         return {
-            shape = {
-                {0, 0, 0, 0},
-                {0, 1, 1, 0},
-                {0, 1, 1, 0},
-                {0, 0, 0, 0}
-            },
-            position = {x = 4, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 4, y = 0, z = 0}
         }
     end
 
     function new_S_PIECE()
+        local shape = {
+            {0, 0, 0},
+            {0, 1, 1},
+            {1, 1, 0}
+        }
+
         return {
-            shape = {
-                {0, 0, 0},
-                {0, 1, 1},
-                {1, 1, 0}
-            },
-            position = {x = 5, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 5, y = 0, z = 0}
         }
     end
 
     function new_L_PIECE()
+        local shape = {
+            {0, 0, 0},
+            {1, 1, 1},
+            {1, 0, 0}
+        }
+
         return {
-            shape = {
-                {0, 0, 0},
-                {1, 1, 1},
-                {1, 0, 0}
-            },
-            position = {x = 5, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 5, y = 0, z = 0}
         }
     end
 
     function new_I_PIECE()
+        local shape = {
+            {0, 0, 0, 0},
+            {1, 1, 1, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        }
+
         return {
-            shape = {
-                {0, 0, 0, 0},
-                {1, 1, 1, 1},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0}
-            },
-            position = {x = 4, y = 0}
+            shape = get_shape3D(shape),
+            position = {x = 4, y = 0, z = 0}
         }
     end
 
     current_piece = new_I_PIECE()
-    instantiate_piece(board, current_piece)
+    set_piece(board, current_piece)
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
-    for i = 1, COLUMNS + 1 do
-        love.graphics.line(i * SCALED_GRID, 0, i * SCALED_GRID, HEIGHT * SCALED_GRID)
-    end
-    for i = 1, ROWS + 1 do
-        love.graphics.line(0, i * SCALED_GRID, WIDTH * SCALED_GRID, i * SCALED_GRID)
+    for depth = 0, DEPTH - 1 do
+        local x_offset = depth * (COLUMNS * SCALED_GRID + PADDING) + PADDING
+        local y_offset = PADDING
+
+        -- Draw horizontal lines
+        for row = 0, ROWS do
+            local y = y_offset + (row * SCALED_GRID)
+            love.graphics.line(x_offset, y, x_offset + COLUMNS * SCALED_GRID, y)
+        end
+
+        -- Draw vertical lines
+        for column = 0, COLUMNS do
+            local x = x_offset + (column * SCALED_GRID)
+            love.graphics.line(x, y_offset, x, y_offset + ROWS * SCALED_GRID)
+        end
     end
 
-    for i = 1, #board do
-        for j = 1, #board[i] do
-            if board[i][j] == 1 then
-                love.graphics.setColor(1, 1, 1)
-                love.graphics.rectangle("fill", (j - COLUMNS_OFFSET) * SCALED_GRID, (i - ROWS_OFFSET) * SCALED_GRID, SCALED_GRID, SCALED_GRID)
-            elseif board[i][j] == 2 then
-                love.graphics.setColor(0.25, 0.25, 0.25)
-                love.graphics.rectangle("fill", (j - COLUMNS_OFFSET) * SCALED_GRID, (i - ROWS_OFFSET) * SCALED_GRID, SCALED_GRID, SCALED_GRID)
+    for depth = 1, #board do
+        local x_offset = depth * (COLUMNS * SCALED_GRID + PADDING) + PADDING
+        local y_offset = PADDING
+
+        for row = 1, #board[depth] do
+            for column = 1, #board[depth][row] do
+                if board[depth][row][column] == 1 then
+                    love.graphics.setColor(1, 1, 1)
+                    love.graphics.rectangle("fill", x_offset + ((column - COLUMNS_OFFSET) * SCALED_GRID), y_offset + ((row - ROWS_OFFSET) * SCALED_GRID), SCALED_GRID, SCALED_GRID)
+                elseif board[depth][row][column] == 2 then
+                    love.graphics.setColor(0.25, 0.25, 0.25)
+                    love.graphics.rectangle("fill", x_offset + ((column - COLUMNS_OFFSET) * SCALED_GRID), y_offset + ((row - ROWS_OFFSET) * SCALED_GRID), SCALED_GRID, SCALED_GRID)
+                end
             end
         end
     end
@@ -135,49 +188,54 @@ end
 function line_clear(board)
     local line_clear_count = 0 -- Will be used for scoring
 
-    for i = 1, #board do
-        full_line = true
-        for j = COLUMNS_OFFSET, #board[i] - COLUMNS_BUFFER + 1 do
-            if board[i][j] ~= 2 then
-                full_line = false
-            end
-        end
-
-        if full_line then
-            line_clear_count = line_clear_count + 1
-
-            -- Move all rows above this line down by one row
-            for row = i, 2, -1 do  -- Start from the cleared line and move upward
-                for col = 1, #board[row] do
-                    board[row][col] = board[row-1][col]  -- Copy from row above
+    for depth = 1, #board do
+        for row = 1, #board[depth] do
+            full_line = true
+            for column = COLUMNS_OFFSET, #board[depth][row] - COLUMNS_BUFFER + 1 do
+                if board[depth][row][column] ~= 2 then
+                    full_line = false
                 end
             end
-            
-            -- Don't increment i since we need to check the same row again
-            -- (since we just moved a new row into this position)
-            i = i - 1
+
+            if full_line then
+                line_clear_count = line_clear_count + 1
+
+                -- Move all rows above this line down by one row
+                for i = row, 2, -1 do  -- Start from the cleared line and move upward
+                    for j = 1, #board[i] do
+                        board[i][j] = board[i - 1][j]  -- Copy from row above
+                    end
+                end
+                
+                -- Don't increment i since we need to check the same row again
+                -- (since we just moved a new row into this position)
+                row = row - 1
+            end
         end
     end
 end
 
 function is_valid_position(board, piece)
-    for i = 1, #piece.shape do
-        for j = 1, #piece.shape[i] do
-            if piece.shape[i][j] == 1 then
-                -- Check for vertical bounds
-                if piece.position.y + i > ROWS + ROWS_BUFFER - ROWS_OFFSET then
-                    return false
-                end
+    for depth = 1, #piece.shape do
+        for row = 1, #piece.shape[depth] do
+            for column = 1, #piece.shape[depth][row] do
 
-                -- Check for horizontal bounds
-                if piece.position.x + j > COLUMNS + COLUMNS_BUFFER - COLUMNS_OFFSET or 
-                   piece.position.x + j <= COLUMNS_BUFFER - COLUMNS_OFFSET then
-                    return false
-                end
-                
-                -- Check for collision with placed pieces
-                if board[piece.position.y + i][piece.position.x + j] == 2 then
-                    return false
+                if piece.shape[depth][row][column] == 1 then
+                    -- Check for vertical bounds
+                    if piece.position.y + row > ROWS + ROWS_BUFFER - ROWS_OFFSET then
+                        return false
+                    end
+
+                    -- Check for horizontal bounds
+                    if piece.position.x + column > COLUMNS + COLUMNS_BUFFER - COLUMNS_OFFSET or 
+                    piece.position.x + column <= COLUMNS_BUFFER - COLUMNS_OFFSET then
+                        return false
+                    end
+                    
+                    -- Check for collision with placed pieces
+                    if board[depth][piece.position.y + i][piece.position.x + j] == 2 then
+                        return false
+                    end
                 end
             end
         end
@@ -186,16 +244,17 @@ function is_valid_position(board, piece)
     return true
 end
 
-function instantiate_piece(board, piece)
-    for i = 1, #piece.shape do
-        for j = 1, #piece.shape[i] do
-            if board[piece.position.y + i][piece.position.x + j] == 0 and piece.shape[i][j] == 1 then
-                board[piece.position.y + i][piece.position.x + j] = 1
+function set_piece(board, piece)
+    for depth = 1, #piece.shape do
+        for row = 1, #piece.shape[depth] do
+            for column = 1, #piece.shape[depth][row] do
+                if board[depth][piece.position.y + row][piece.position.x + column] == 0 and piece.shape[depth][row][column] == 1 then
+                    board[depth][piece.position.y + row][piece.position.x + column] = 1
+                end
             end
         end
     end
 end
-
 
 function new_piece()
     -- Debug
@@ -219,28 +278,31 @@ function new_piece()
     --     current_piece = new_I_PIECE()
     -- end
 
-    instantiate_piece(board, current_piece)
+    set_piece(board, current_piece)
 end
 
 function place_piece(board, piece)
-    for i = 1, #piece.shape do
-        for j = 1, #piece.shape[i] do
-            if board[piece.position.y + i][piece.position.x + j] == 0 and piece.shape[i][j] == 1 then
-                board[piece.position.y + i][piece.position.x + j] = 2
+    for depth = 1, #piece.shape do
+        for row = 1, #piece.shape[depth] do
+            for column = 1, #piece.shape[depth][row] do
+                if board[depth][piece.position.y + row][piece.position.x + column] == 0 and piece.shape[depth][row][column] == 1 then
+                    board[depth][piece.position.y + row][piece.position.x + column] = 2
+                end
             end
         end
     end
 
     line_clear(board)
-
     new_piece()
 end
 
 function clear_piece(board, piece)
-    for i = 1, #piece.shape do
-        for j = 1, #piece.shape[i] do
-            if board[piece.position.y + i][piece.position.x + j] == 1 then
-                board[piece.position.y + i][piece.position.x + j] = 0
+    for depth = 1, #piece.shape do 
+        for row = 1, #piece.shape[depth] do
+            for column = 1, #piece.shape[depth][row] do
+                if board[depth][piece.position.y + row][piece.position.x + column] == 1 then
+                    board[depth][piece.position.y + row][piece.position.x + column] = 0
+                end
             end
         end
     end
@@ -258,7 +320,7 @@ function lower_piece(board, piece)
         return
     end
 
-    instantiate_piece(board, piece)
+    set_piece(board, piece)
 end
 
 function shift_piece(board, piece, dir)
@@ -271,7 +333,7 @@ function shift_piece(board, piece, dir)
         piece.position.x = original_x
     end
 
-    instantiate_piece(board, piece)
+    set_piece(board, piece)
 end
 
 function rotate_piece(board, piece, dir)
@@ -329,7 +391,7 @@ function rotate_piece(board, piece, dir)
 
     -- Clear and redraw board with rotated piece
     clear_piece(board, piece)
-    instantiate_piece(board, piece)
+    set_piece(board, piece)
 end
 
 
